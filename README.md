ce64e3a2f25a592e1f074503d3896157492ea0b9
iamzhz <zhzqqyd@qq.com>
2024-01-31 15:01:27 +0800

    rebuilding

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # TypingPractice
-## 编译
+A simple typing practice program running on Windows.  
+一个运行在Windows的简单打字练习程序
+## Compile 编译
 ```
 gcc tp.c mycon.c -o tp.exe -lwinmm
 ```
-## 运行
-把需要练习打字的文字存储在当前目录的1.txt文件
+## Run 运行
+Store the text that needs to be practiced typing in the `1. txt` file of the current directory.  
+把需要练习打字的文字存储在当前目录的`1.txt`文件
 ## :link:
 [iamzhz/mycon](https://github.com/iamzhz/mycon)
--- tp.c
+++ tp.c
@@ -2,67 +2,47 @@
 #include <windows.h>
 #include <stdlib.h>
 #include "mycon.h"
-int main(void)
-{
+int main(void) {
+	int ch, get;
+	int type = 0;
+	int curLine = 2;
+	FILE *fp = fopen("1.txt", "r");
 	cls();
-	
-	FILE *fp;
-	fp=fopen("1.txt", "r");
-	
-	set_size(30,100);                 // 宽=50，高=30 
-	set_title("打字练习");
-	system("color 3F");              // 2=绿色，F=白色 
-	gotoxy(2,1);                     // x=2,y=3
-	
-	char a;                          // a=接收的内容 
-	char b;                          // b=文件里的一个字符 
-	int c=0 ,d=0 ,f=0;               // c=错了几个 ,d=对了几个，f=一共几个 
-	
-	 
-	//从下一行 
-	b=fgetc(fp);
-	while(b != EOF)
-	{
-		f++ ;                        // f=一共几个 
-		printf("%c", b);
-		b=fgetc(fp);
+	gotoxy(4, curLine);
+	set_size(100, 30);
+	if(fp == NULL) {
+		printf("[ERROR] Cannot open file `1.txt`!\n");
+		fclose(fp);
+		while(1);
 	}
-	//到上一行  是显示文件内容
-	
-	gotoxy(2,2);                     // x=3,y=3
-	
-	rewind(fp);                      // fp - 首位
-	
-	set_color(3,5);
-	
-	//从下一行 
-	b=fgetc(fp);
-	while(b != EOF)
-	{
-		a=getkey();
-		printf("%c", a);
-		
-		if(a != b)   c++ ;            // c=错了几个 ,d=对了几个
-		else         d++ ;
-		
-		b=fgetc(fp);
+	ch = fgetc(fp);
+	while(!feof(fp)) {
+		if(ch == '\n') {
+			curLine ++;
+			gotoxy(3, curLine);
+		}else{
+			printf("%c", ch);
+		}
+		ch = fgetc(fp);
 	}
-	//到上一行  是接收和判断
-	
-	set_color(3,0xF);
-	
-	if(c == 0)
-	{
-		printf("\n\n\nVery good!");
-		goto E;
+	curLine = 2;
+	rewind(fp);
+	ch = fgetc(fp);
+	gotoxy(3, curLine);
+	while(!feof(fp)) {
+		if(ch == '\n') {
+			curLine ++;
+			printf("\b ");
+			gotoxy(3, curLine);
+		}else{
+			while(ch != getkey()) type ++;
+			type ++;
+			printf("\b |");
+		}
+		ch = fgetc(fp);
 	}
-	
-	printf("\n\n\n一共%d 个，你错了%d 个，对了%d 个。",f ,c ,d);
-	
-	E:
-	
-	while(1)
-	Sleep(500);
-	
-	return 0;
+	gotoxy(4, 2);
+	printf("Great. Pressed the button %d times!\n    Please click `X` to exit.", type);
+	fclose(fp);
+	while(1);
 }
